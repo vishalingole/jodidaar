5795f68fec38b3a310734d8449b9ed99777eafdf
prashyaa <prashantbajad555@gmail.com>
2024-02-07 16:08:11 +0530

    changes for imggrid

---
+++ src/components/ImageGallery/index.js
@@ -0,0 +1,73 @@
+import "./index.css";
+import React from "react";
+
+const ImageGallery = () => {
+  return (
+    <>
+      <div className="profile-image-grid">
+        <div className="grid-item">
+          {" "}
+          <img
+            src="https://images.unsplash.com/photo-1682687220742-aba13b6e50ba?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8"
+            alt=""
+          />
+          <div className="card-overlay">
+            {" "}
+            <div>Title_1 </div>{" "}
+            <div>Lorem ipsum dolor sit amet consectetur.</div>{" "}
+          </div>
+        </div>
+        <div className="grid-item">
+          {" "}
+          <img
+            src="https://images.unsplash.com/photo-1682687220742-aba13b6e50ba?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8"
+            alt=""
+          />
+          <div className="card-overlay">
+            {" "}
+            <div>Title_1 </div>{" "}
+            <div>Lorem ipsum dolor sit amet consectetur.</div>{" "}
+          </div>
+        </div>
+        <div className="grid-item">
+          {" "}
+          <img
+            src="https://images.unsplash.com/photo-1682687220742-aba13b6e50ba?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8"
+            alt=""
+          />
+          <div className="card-overlay">
+            {" "}
+            <div>Title_1 </div>{" "}
+            <div>Lorem ipsum dolor sit amet consectetur.</div>{" "}
+          </div>
+        </div>
+        <div className="grid-item">
+          {" "}
+          <img
+            src="https://images.unsplash.com/photo-1682687220742-aba13b6e50ba?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8"
+            alt=""
+          />
+          <div className="card-overlay">
+            {" "}
+            <div>Title_1 </div>{" "}
+            <div>Lorem ipsum dolor sit amet consectetur.</div>{" "}
+          </div>
+        </div>
+        <div className="grid-item">
+          {" "}
+          <img
+            src="https://images.unsplash.com/photo-1682687220742-aba13b6e50ba?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8"
+            alt=""
+          />
+          <div className="card-overlay">
+            {" "}
+            <div>Title_1 </div>{" "}
+            <div>Lorem ipsum dolor sit amet consectetur.</div>{" "}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ImageGallery;
