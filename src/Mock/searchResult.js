9761ac143392e990ad071588166d345bf7199461
vishal ingole <vishal@ranvijaysatyajeetgmail.onmicrosoft.com>
2024-02-18 02:05:10 -0800

    added changes

--- src/Mock/searchResult.js
+++ src/Mock/searchResult.js
@@ -1,7 +1,241 @@
 export const searchResultMock = {
-  totalItems: 3,
+  totalItems: 12,
   data: {
     items: [
+      {
+        uuid: "89a3a820-afa6-11ee-92fc-cf4595260aaa",
+        PersonalDetails: {
+          lastName: "Ingole",
+          gender: "Male",
+          dob: null,
+          displayId: "G1",
+          height: 5.5,
+        },
+        EducationDetails: {
+          occupationPlace: "Pune",
+          occupationDetail: "Software Engineer",
+          occupationType: "private",
+          educationArea: "postGraduate",
+          education: "Master of computer application",
+          income: 100000,
+          incomeType: "Annual",
+        },
+        ProfileImage: {
+          fileName: "public\\uploads\\file-1704884189495.jpg",
+        },
+        FamilyBackground: {
+          nativeDistrict: "Washim",
+        },
+        file: "",
+      },
+      {
+        uuid: "e613c450-afc4-11ee-b021-c1f511c662df",
+        PersonalDetails: {
+          lastName: "Patil",
+          gender: "Female",
+          dob: null,
+          displayId: "B3",
+          height: 5,
+        },
+        EducationDetails: {
+          occupationPlace: "Banglore",
+          occupationDetail: "Private Service",
+          occupationType: "private",
+          educationArea: "graduate",
+          education: "BSC",
+          income: 20000,
+          incomeType: "Monthly",
+        },
+        ProfileImage: {
+          fileName: "public\\uploads\\file-1704897317995.jpg",
+        },
+        FamilyBackground: {
+          nativeDistrict: "Washim",
+        },
+        file: "",
+      },
+      {
+        uuid: "8f9afeb0-b396-11ee-b10d-3fa94ec32c39",
+        PersonalDetails: {
+          lastName: "Bajad",
+          gender: "Male",
+          dob: null,
+          displayId: "G4",
+          height: 5.5,
+        },
+        EducationDetails: {
+          occupationPlace: "Pune",
+          occupationDetail: "Electronics Engineer",
+          occupationType: "private",
+          educationArea: "graduate",
+          education: "BE",
+          income: 3000000,
+          incomeType: "Annual",
+        },
+        ProfileImage: {
+          fileName: "public\\uploads\\file-1705317202344.jpg",
+        },
+        FamilyBackground: {
+          nativeDistrict: "Washim",
+        },
+        file: "",
+      },
+      {
+        uuid: "89a3a820-afa6-11ee-92fc-cf4595260aaa",
+        PersonalDetails: {
+          lastName: "Ingole",
+          gender: "Male",
+          dob: null,
+          displayId: "G1",
+          height: 5.5,
+        },
+        EducationDetails: {
+          occupationPlace: "Pune",
+          occupationDetail: "Software Engineer",
+          occupationType: "private",
+          educationArea: "postGraduate",
+          education: "Master of computer application",
+          income: 100000,
+          incomeType: "Annual",
+        },
+        ProfileImage: {
+          fileName: "public\\uploads\\file-1704884189495.jpg",
+        },
+        FamilyBackground: {
+          nativeDistrict: "Washim",
+        },
+        file: "",
+      },
+      {
+        uuid: "e613c450-afc4-11ee-b021-c1f511c662df",
+        PersonalDetails: {
+          lastName: "Patil",
+          gender: "Female",
+          dob: null,
+          displayId: "B3",
+          height: 5,
+        },
+        EducationDetails: {
+          occupationPlace: "Banglore",
+          occupationDetail: "Private Service",
+          occupationType: "private",
+          educationArea: "graduate",
+          education: "BSC",
+          income: 20000,
+          incomeType: "Monthly",
+        },
+        ProfileImage: {
+          fileName: "public\\uploads\\file-1704897317995.jpg",
+        },
+        FamilyBackground: {
+          nativeDistrict: "Washim",
+        },
+        file: "",
+      },
+      {
+        uuid: "8f9afeb0-b396-11ee-b10d-3fa94ec32c39",
+        PersonalDetails: {
+          lastName: "Bajad",
+          gender: "Male",
+          dob: null,
+          displayId: "G4",
+          height: 5.5,
+        },
+        EducationDetails: {
+          occupationPlace: "Pune",
+          occupationDetail: "Electronics Engineer",
+          occupationType: "private",
+          educationArea: "graduate",
+          education: "BE",
+          income: 3000000,
+          incomeType: "Annual",
+        },
+        ProfileImage: {
+          fileName: "public\\uploads\\file-1705317202344.jpg",
+        },
+        FamilyBackground: {
+          nativeDistrict: "Washim",
+        },
+        file: "",
+      },
+      {
+        uuid: "89a3a820-afa6-11ee-92fc-cf4595260aaa",
+        PersonalDetails: {
+          lastName: "Ingole",
+          gender: "Male",
+          dob: null,
+          displayId: "G1",
+          height: 5.5,
+        },
+        EducationDetails: {
+          occupationPlace: "Pune",
+          occupationDetail: "Software Engineer",
+          occupationType: "private",
+          educationArea: "postGraduate",
+          education: "Master of computer application",
+          income: 100000,
+          incomeType: "Annual",
+        },
+        ProfileImage: {
+          fileName: "public\\uploads\\file-1704884189495.jpg",
+        },
+        FamilyBackground: {
+          nativeDistrict: "Washim",
+        },
+        file: "",
+      },
+      {
+        uuid: "e613c450-afc4-11ee-b021-c1f511c662df",
+        PersonalDetails: {
+          lastName: "Patil",
+          gender: "Female",
+          dob: null,
+          displayId: "B3",
+          height: 5,
+        },
+        EducationDetails: {
+          occupationPlace: "Banglore",
+          occupationDetail: "Private Service",
+          occupationType: "private",
+          educationArea: "graduate",
+          education: "BSC",
+          income: 20000,
+          incomeType: "Monthly",
+        },
+        ProfileImage: {
+          fileName: "public\\uploads\\file-1704897317995.jpg",
+        },
+        FamilyBackground: {
+          nativeDistrict: "Washim",
+        },
+        file: "",
+      },
+      {
+        uuid: "8f9afeb0-b396-11ee-b10d-3fa94ec32c39",
+        PersonalDetails: {
+          lastName: "Bajad",
+          gender: "Male",
+          dob: null,
+          displayId: "G4",
+          height: 5.5,
+        },
+        EducationDetails: {
+          occupationPlace: "Pune",
+          occupationDetail: "Electronics Engineer",
+          occupationType: "private",
+          educationArea: "graduate",
+          education: "BE",
+          income: 3000000,
+          incomeType: "Annual",
+        },
+        ProfileImage: {
+          fileName: "public\\uploads\\file-1705317202344.jpg",
+        },
+        FamilyBackground: {
+          nativeDistrict: "Washim",
+        },
+        file: "",
+      },
       {
         uuid: "89a3a820-afa6-11ee-92fc-cf4595260aaa",
         PersonalDetails: {
